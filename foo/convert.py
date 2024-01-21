5b6d9cdc4560ccf8a55e9245d7a860c7ec9b3b1a
SteffRhes <stefanresch@protonmail.com>
2024-01-21 17:36:52 +0100

    prefect deployment experiment

---
+++ foo/convert.py
@@ -0,0 +1,46 @@
+import xml.etree.ElementTree as ET
+import xml.dom.minidom as minidom
+import json
+from prefect import flow
+
+
+file_path_input = "./foo/data.json"
+file_path_output = "./foo/data.xml"
+
+
+def dict_to_xml(d, node_current=None):
+    if node_current is None:
+        node_current = ET.Element("root")
+    k_v_pairs = None
+    if type(d) is dict:
+        k_v_pairs = [(ET.Element(k), v) for k, v in d.items()]
+    elif type(d) is list:
+        k_v_pairs = [(ET.Element("li"), v) for v in d]
+    for node_k, v in k_v_pairs:
+        if type(v) is dict or type(v) is list:
+            dict_to_xml(v, node_k)
+        else:
+            node_k.text = str(v)
+        node_current.append(node_k)
+    return node_current
+
+
+@flow(name="veld_executable_ex2_json_to_xml__yaml", log_prints=True)
+def convert():
+    print("loading from:", file_path_input)
+    with open(file_path_input, "r") as fi:
+        d = json.load(fi)
+        print("original json data:")
+        print(d)
+        xml_content = dict_to_xml(d)
+        xml_str = minidom.parseString(ET.tostring(xml_content)).toprettyxml(indent="  ")
+        print("converted xml data:")
+        print(xml_str)
+        print("writing to", file_path_output)
+        with open(file_path_output, "w") as fo:
+            fo.write(xml_str)
+
+
+if __name__ == "__main__":
+    convert()
+
